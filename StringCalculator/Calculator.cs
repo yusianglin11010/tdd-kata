057c5df08dd04696c46b3e361142cb6f135d66a9
devin_lin <devin_lin@gss.com.tw>
2024-07-01 23:39:13 +0800

    String Calculator

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StringCalculator;
+
+public static class Calculator
+{
+    public static int Calculate(string input)
+    {
+        var delimiter = ",";
+        var delimiterMatcher = new Regex(@"(?s)\/\/(?<delimiter>.)(?<nums>.*)");
+        string nums;
+        if (delimiterMatcher.IsMatch(input))
+        {
+            delimiter = delimiterMatcher.Match(input).Groups["delimiter"].Value;
+            nums = delimiterMatcher.Match(input).Groups["nums"].Value;
+        }
+        else
+        {
+            nums = input;
+        }
+        
+        nums = nums.Replace("\n", delimiter);
+        if (!string.IsNullOrWhiteSpace(nums))
+        {
+            var inputStrings = nums.Split(delimiter);
+            if (inputStrings.Any(x => x.Equals(string.Empty)))
+            {
+                throw new ArgumentException(nameof(nums));
+            }
+            
+            var inputNums = inputStrings
+                .Select(x => int.TryParse(x, out var result) ? result : 0)
+                .Where(x => x <= 1000)
+                .ToList();
+            var negativesNums = inputNums.Where(x => x < 0).ToList();
+            if (negativesNums.Count > 0)
+            {
+                throw new ArgumentException($"Negatives not allowed: {string.Join(",", negativesNums)}");
+            }
+            
+            return inputNums.Sum();
+        };
+        return 0;
+    }
+}
---
+++ StringCalculator/TestCalculator.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Xunit;
+
+namespace StringCalculator
+{
+    public class TestCalculator
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        [InlineData("", 0)]
+        public void Calculate(string input, int expected)
+        {
+            var result = Calculator.Calculate(input);
+            Assert.Equal(expected, result);
+        }
+        
+        [Theory]
+        [InlineData("1,2,3,4,5", 15)]
+        [InlineData("1,2,3,4", 10)]
+        [InlineData("1,2,3", 6)]
+        public void CalculateUnknownNumber(string input, int expected)
+        {
+            var result = Calculator.Calculate(input);
+            Assert.Equal(expected, result);
+        }
+        
+        [Theory]
+        [InlineData("1\n2,3,4,5", 15)]
+        [InlineData("1\n2\n3,4,5", 15)]
+        [InlineData("1\n2\n3\n4,5", 15)]
+        [InlineData("1\n2\n3\n4\n5", 15)]
+        public void CalculateUnknownNumberWithNewLines(string input, int expected)
+        {
+            var result = Calculator.Calculate(input);
+            Assert.Equal(expected, result);
+        }
+        
+        [Fact]
+        public void CalculateUnknownNumberInvalidInput()
+        {
+            var input = "1\n,";
+            Assert.Throws<ArgumentException>(() => Calculator.Calculate(input));
+        }
+        
+        [Theory]
+        [InlineData("//,1,2,3,4,5", 15)]
+        [InlineData("//,1\n2,3,4,5", 15)]
+        [InlineData("//;1\n2;3;4;5", 15)]
+        [InlineData("//'1\n2'3'4'5", 15)]
+        public void CalculateWithCustomDelimiter(string input, int expected)
+        {
+            var result = Calculator.Calculate(input);
+            Assert.Equal(expected, result);
+        }
+        
+        [Theory]
+        [InlineData("//;-1;-2")]
+        [InlineData("-1,-2")]
+        [InlineData("-1\n-2")]
+        public void CalculateWithNegative(string input)
+        {
+            const string expectedMessage = "Negatives not allowed: -1,-2";
+            var exception = Assert.Throws<ArgumentException>(() => Calculator.Calculate(input));
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+        
+        [Theory]
+        [InlineData("1,1001",1)]
+        [InlineData("1,2000",1)]
+        [InlineData("1,1000",1001)]
+
+        public void CalculateWithNumberGreaterThan1000(string input, int expected)
+        {
+            var result = Calculator.Calculate(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
