be889679b33dcdc80623e073cf4900bf404ea268
devin_lin <devin_lin@gss.com.tw>
2024-07-02 23:05:35 +0800

    Add FizzBuzz

---
+++ FizzBuzz/FizzBuzzPrinter.cs
@@ -0,0 +1,48 @@
+﻿namespace FizzBuzz;
+
+public static class FizzBuzzPrinter
+{
+    public const string BuzzString = "Buzz";
+    public const string FizzString = "Fizz";
+    public const string FizzBuzzString = "FizzBuzz";
+    
+    public static string Print(int input)
+    {
+        if (input is > 0 and <= 100)
+        {
+            if (IsDividedByThreeAndFine(input))
+            {
+                return FizzBuzzString;
+            }
+            
+            if (IsDividedByThree(input))
+            {
+                return FizzString;
+            }
+
+            if (IsDividedByFive(input))
+            {
+                return BuzzString;
+            }
+
+            return string.Empty;
+        }
+
+        throw new ArgumentException(nameof(input));
+    }
+
+    private static bool IsDividedByThree(int input)
+    {
+        return input % 3 == 0;
+    }
+    
+    private static bool IsDividedByFive(int input)
+    {
+        return input % 5 == 0;
+    }
+    
+    private static bool IsDividedByThreeAndFine(int input)
+    {
+        return IsDividedByThree(input) && IsDividedByFive(input);
+    }
+}
---
+++ FizzBuzz/FizzBuzzPrinterTests.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+
+namespace FizzBuzz;
+
+public class FizzBuzzPrinterTests
+{
+    [Theory]
+    [InlineData(3)]
+    [InlineData(6)]
+    [InlineData(9)]
+    [InlineData(21)]
+    [InlineData(99)]
+    public void FizzBuzzPrintFizz(int input)
+    {
+        Assert.Equal("Fizz", FizzBuzzPrinter.Print(input));
+    }
+    
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(11)]
+    [InlineData(8)]
+    public void FizzBuzzPrintNothing(int input)
+    {
+        Assert.Equal(string.Empty, FizzBuzzPrinter.Print(input));
+    }
+    
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(101)]
+    public void FizzBuzzInvalidInput(int input)
+    {
+        Assert.Throws<ArgumentException>(() => FizzBuzzPrinter.Print(input));
+    }
+    
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+    [InlineData(20)]
+    [InlineData(25)]
+    [InlineData(35)]
+    public void FizzBuzzPrintBuzz(int input)
+    {
+        Assert.Equal("Buzz", FizzBuzzPrinter.Print(input));
+    }
+    
+    [Theory]
+    [InlineData(15)]
+    [InlineData(30)]
+    [InlineData(45)]
+    [InlineData(60)]
+    public void FizzBuzzPrintFizzBuzz(int input)
+    {
+        Assert.Equal("FizzBuzz", FizzBuzzPrinter.Print(input));
+    }
+}
