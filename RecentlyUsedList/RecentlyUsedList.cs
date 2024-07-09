a1c907ed86b7bf23c9462ee1122517eaed7861bf
devin_lin <devin_lin@gss.com.tw>
2024-07-09 23:16:38 +0800

    Add RecentlyUsedList

---
+++ RecentlyUsedList/RecentlyUsedList.cs
@@ -0,0 +1,85 @@
+﻿namespace RecentlyUsedList;
+
+public class RecentlyUsedList : List<string>
+{
+    private List<string> Values { get; } = [];
+    private ISet<string> UniqueSet { get; } = new HashSet<string>();
+
+    private readonly int _maxSize;
+    
+    public RecentlyUsedList(int maxSize = 5)
+    {
+        _maxSize = maxSize;
+    }
+    
+    public new object this[int i]
+    {
+        get
+        {
+            if (i >= Length())
+            {
+                throw new IndexOutOfRangeException($"Index {i} is out of range");
+            }
+
+            if (i < 0)
+            {
+                throw new ArgumentException("Negative index is not allowed");
+            }
+            
+            return i < 0
+                // -1 -> 0 = -(-1) -1
+                // -2 -> 1 = -(-2) - 1
+                // -3 -> 2 = -(-3) - 1
+                ? Values[-i - 1]
+                : Values[Values.Count - i - 1];
+        }
+    }
+
+    public int GetSize()
+    {
+        return _maxSize;
+    }
+    
+    public string? First()
+    {
+        return Values.ElementAtOrDefault(Length() - 1);
+    }
+
+    public string? Last()
+    {
+        return Values.ElementAtOrDefault(0);
+    }
+
+    public void AddItem(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new InvalidOperationException(nameof(input));
+        }
+        
+        if (UniqueSet.Contains(input))
+        {
+            Values.Remove(input);
+        }
+        else
+        {
+            UniqueSet.Add(input);
+        }
+        
+        EnsureEnoughCapacity();
+        Values.Add(input);
+    }
+
+    public int Length()
+    {
+        return Values.Count;
+    }
+
+    private void EnsureEnoughCapacity()
+    {
+        if (Values.Count >= _maxSize)
+        {
+            Values.Remove(Last());
+        }
+    }
+}
---
+++ RecentlyUsedList/RecentlyUsedListTests.cs
@@ -0,0 +1,113 @@
+﻿using Xunit;
+
+namespace RecentlyUsedList;
+
+public class RecentlyUsedListTests
+{
+    private readonly RecentlyUsedList _recentlyUsedList = new RecentlyUsedList();
+    private const string First = "first";
+    private const string Second = "second";
+    private const string Last = "last";
+    
+    [Fact]
+    public void RecentlyAddFirst()
+    {
+        _recentlyUsedList.AddItem(First);
+        _recentlyUsedList.AddItem(Second);
+        _recentlyUsedList.AddItem(Last);
+        
+        Assert.Equal(Last, _recentlyUsedList.First());
+        Assert.Equal(First, _recentlyUsedList.Last());
+    }
+
+    [Fact]
+    public void RetrieveDataWithIndex()
+    {
+        _recentlyUsedList.AddItem(First);
+        _recentlyUsedList.AddItem(Second);
+        _recentlyUsedList.AddItem(Last);
+        
+        Assert.Equal(Last, _recentlyUsedList[0]);
+        Assert.Equal(Second, _recentlyUsedList[1]);
+        Assert.Equal(First, _recentlyUsedList[2]);
+    }
+    
+    [Fact]
+    public void DuplicateAdd()
+    {
+        _recentlyUsedList.AddItem(First);
+        _recentlyUsedList.AddItem(Second);
+        _recentlyUsedList.AddItem(Last);
+        _recentlyUsedList.AddItem(Second);
+
+        
+        Assert.Equal(Second, _recentlyUsedList[0]);
+        Assert.Equal(Last, _recentlyUsedList[1]);
+        Assert.Equal(First, _recentlyUsedList[2]);
+    }
+
+    [Fact]
+    public void InitializedAsEmpty()
+    {
+        var recentlyUsedList = new RecentlyUsedList();
+        Assert.Empty(recentlyUsedList);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void AssertNullOrEmptyString(string item)
+    {
+        Assert.Throws<InvalidOperationException>(() => _recentlyUsedList.AddItem(item));
+    }
+    
+    [Fact]
+    public void SizeLimit()
+    {
+        const int maxSize = 1;
+        var recentlyUsedList = new RecentlyUsedList(maxSize);
+        var existedItem = "0";
+        recentlyUsedList.AddItem(existedItem);
+        var addedItem = "123";
+        recentlyUsedList.AddItem(addedItem);
+        
+        Assert.Equal(maxSize, recentlyUsedList.Length());
+        Assert.Equal(recentlyUsedList.First(), addedItem);
+    }
+    
+    [Theory]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void OutOfRangeIndex(int index)
+    {
+        _recentlyUsedList.AddItem(First);
+        _recentlyUsedList.AddItem(Second);
+
+        Assert.Throws<IndexOutOfRangeException>(() => _recentlyUsedList[index]);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    public void NotAllowedNegativeIndex(int index)
+    {
+        _recentlyUsedList.AddItem(First);
+        _recentlyUsedList.AddItem(Second);
+
+        Assert.Throws<ArgumentException>(() => _recentlyUsedList[index]);
+    }
+    
+    [Fact]
+    public void DefaultSizeLimitToFive()
+    {
+        var recentlyUsedList = new RecentlyUsedList();
+        for (var i = 0; i < 5; i++)
+        {
+            recentlyUsedList.AddItem(i.ToString());
+        }
+        
+        recentlyUsedList.AddItem(Last);
+        Assert.Equal(5, recentlyUsedList.GetSize());
+        Assert.Equal(Last, recentlyUsedList.First());
+    }
+}
